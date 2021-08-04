4549f1f072f5b0912fcb46dd7f6e774c0c0d96ba
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-08-04 18:45:31 +0100

    add the function of userprofile and edit profile

--- rango/urls.py
+++ rango/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from rango import views
+from rango.views import AboutView
 
 app_name="rango"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    #update
+    path('about/', views.AboutView.as_view(), name='about'),
     path('forum/', views.forum, name='forum'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
-    path('add_category/', views.add_category, name='add_category'),
+    #update
+    path('add_category/', views.AddCategoryView.as_view(), name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('category/<slug:category_name_slug>/add_video/', views.add_video, name='add_video'),
     path('restricted/', views.restricted, name='restricted'),
@@ -18,4 +21,6 @@ urlpatterns = [
     path('add_comment/', views.add_comment, name='add_comment'),
     path('courses/', views.courses, name='courses'),
     path('courses/<course_id>/', views.single_course, name='single_course'),
+    path('register_profile/', views.register_profile, name='register_profile'),
+    path('profile/<username>/', views.ProfileView.as_view(), name='profile'),
 ]
