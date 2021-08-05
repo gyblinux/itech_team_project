716da91cf8801f31c5370ee9db0dccaaa5e9cef3
Lei Zhou <2527467z@student.gla.ac.uk>
2021-08-06 00:04:16 +0800

    fix_bug&add_search

--- rango/urls.py
+++ rango/urls.py
@@ -10,13 +10,9 @@ urlpatterns = [
     path('forum/', views.forum, name='forum'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/<course_id>', views.add_category, name='add_category'),
-    # path('add_category/', views.AddCategoryView.as_view(), name='add_category'), -> for the convenience of adding new categories
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('category/<slug:category_name_slug>/add_video/', views.add_video, name='add_video'),
     path('restricted/', views.restricted, name='restricted'),
-    # path('register/', views.register, name='register'),
-    # path('login/', views.user_login, name='login'),
-    # path('logout/', views.user_logout, name='logout'),
     path('add_comment/', views.add_comment, name='add_comment'),
     path('courses/', views.courses, name='courses'),
     path('courses/<course_id>/', views.single_course, name='single_course'),
@@ -26,4 +22,5 @@ urlpatterns = [
 
     # new
     path('courses/<course_id>/<slug:category_name_slug>/', views.single_category, name='single_category'),
+    path('search/', views.search, name='search')
 ]
